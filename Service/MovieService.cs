d8fce29536f58f82e950484f77e1550b5f299be6
BobbydpStrange <51657838+BobbydpStrange@users.noreply.github.com>
2022-11-12 22:12:56 -0700

    added webpagess and logo for api, and models

--- Service/MovieService.cs
+++ Service/MovieService.cs
@@ -1,5 +1,9 @@
 ﻿using System.Net.Http.Json;
 using webProject1.Pages;
+using static webProject1.Model.ActorModel;
+using static webProject1.Model.DiscoverModel;
+using static webProject1.Model.trendingModel;
+using Discover = webProject1.Model.DiscoverModel.Discover;
 
 namespace webProject1.Model
 {
@@ -29,6 +33,35 @@ namespace webProject1.Model
         {
             var apiData = await client.GetFromJsonAsync<MovieInfo>("https://api.themoviedb.org/3/movie/550?api_key=989763942fa4f236cb34de985f499dc6");
             return apiData.title;
+        }
+        public async Task<IEnumerable<Result>> GetTrendingMovieAsync()
+        {
+            //var arrayOfTrendingMovies = new List<MovieInfo>();      
+            var arrayOfTrendingMovies = await client.GetFromJsonAsync<Collection>("https://api.themoviedb.org/3/trending/movie/week?api_key=989763942fa4f236cb34de985f499dc6");
+            return arrayOfTrendingMovies.results;
+        }
+
+        public async Task<IEnumerable<Result>> GetTrendingTvAsync()
+        {
+            var arrayOfTrendingTv = await client.GetFromJsonAsync<Collection>("https://api.themoviedb.org/3/trending/tv/week?api_key=989763942fa4f236cb34de985f499dc6");
+            return arrayOfTrendingTv.results;
+        }
+
+        public async Task<IEnumerable<Actor>> GetPopularActorAsync()
+        {
+            var arrayOfPopularActors = await client.GetFromJsonAsync<Cast>("https://api.themoviedb.org/3/person/popular?api_key=989763942fa4f236cb34de985f499dc6&language=en-US&page=1");
+            return arrayOfPopularActors.results;
+        }
+
+        public async Task<IEnumerable<Output>> GetDiscoverAsync()
+        {
+            var arrayOfDiscover = await client.GetFromJsonAsync<Discover>("https://api.themoviedb.org/3/discover/movie?api_key=989763942fa4f236cb34de985f499dc6&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate");
+            return arrayOfDiscover.results;
+        }
+        public async Task<IEnumerable<Result>> GetPlayingAsync()
+        {
+            var arrayOfPlaying = await client.GetFromJsonAsync<Collection>("https://api.themoviedb.org/3/movie/now_playing?api_key=989763942fa4f236cb34de985f499dc6&language=en-US&page=1");
+            return arrayOfPlaying.results;
         }
     }
 }
